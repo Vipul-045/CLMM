7c2e50681a99c6494cb2bb68dc369b5df96a935b
Vipul-045 <vipulyadav0479@gmail.com>
2025-11-26 16:05:34 +0530

    Initialize pool accounts

--- programs/clmm/src/instructions/pool.rs
+++ programs/clmm/src/instructions/pool.rs
@@ -7,5 +7,44 @@ use crate::utils::math::*;
 #[derive(Accounts)]
 #[intruction(tick_spacing: i32)]
 pub struct InitializePool <'info> {
-    
+    #[account(mut)]
+    pub payer: signer<'info>,
+
+    pub token_mint_1: Account<'info, Mint>,
+    pub token_mint_2: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        payer = payer,
+        space = pool.SPACE,
+        seeds = [
+            b"pool".as_ref(),
+            token_mint_1.key().as_ref(),
+            token_mint_2.key().as_ref(),
+            tick_spacing.to_le_bytes()
+        ],
+        bumps
+    )]
+    pub pool : Account<'info, Pool>
+
+    #[account(
+        init,
+        payer = payer,
+        token::mint = token_mint_1,
+        token::authority = pool,
+    )]
+    pub token_vault_1 : Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = payer,
+        token::mint = token_mint_2,
+        token::authority = pool,
+    )]
+    pub token_vault_2 : Account<'info, TokenAccount>,
+
+    pub system_program : Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent : Sysvar<'info, Rent>
+
 }
