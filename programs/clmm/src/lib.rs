4d79e1bc56f94fe491942a2a85760ca13f779043
Vipul-045 <vipulyadav0479@gmail.com>
2025-12-22 14:21:39 +0530

    updating lib.rs

--- programs/clmm/src/lib.rs
+++ programs/clmm/src/lib.rs
@@ -8,14 +8,26 @@ declare_id!("4YfgeshfgHDeURYNvZH77W4yF8554oenSHAzaAQEmedo");
 
 #[program]
 pub mod clmm {
+    use crate::instructions::open_position::OpenPosition;
+
     use super::*;
 
     pub fn initializepool(ctx: Context<InitializePool>,
         tick_spacing: i32,
         initial_sqrt_price: u128) -> Result<()> {
-        instructions::pool::init_pool(tick_spacing, initial_sqrt_price)
+        instructions::pool::init_pool(ctx, tick_spacing, initial_sqrt_price)
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+    pub fn open_position(
+        ctx: Context<OpenPosition>,
+        owner: Pubkey,
+        lower_tick: i32,
+        upper_tick: i32,
+        liquidity_amount: u128,
+        _tick_array_lower_start_index: i32,
+        _tick_array_upper_start_index: i32) -> Result <(u64, u64)> {
+                instructions::open_position::open_position(ctx, owner, lower_tick, upper_tick, liquidity_amount, tick_array_lower_start_index, tick_array_upper_start_index)
+    }
+
+
+}
